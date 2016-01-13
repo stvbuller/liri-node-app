2269de5bd0df664df499c5bee1b008a20632c902
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-01-13 12:33:09 -0500

    added switch statement to liri.js

---
+++ liri.js
@@ -0,0 +1,19 @@
+var twitterKeys = require("./keys.js");
+
+var parameters = process.argv.slice(3);  //check that this is slicing in the proper position
+
+switch(process.argv[2]) {       //check to see which argv index this should be
+  case "my-tweets":
+    //code to be executed for my-tweets goes here
+    break;
+  case "spotify-this-song":
+    //code for spotify-this-song
+    //if no song is providied default to “what’s my age again” by blink 182
+  case "movie-this":
+    //code for movie-this goes 
+    //if no movie is provided default to ‘Mr. Nobody’
+    break;
+  case "do-what-it-says"
+    //code for do-what-it says
+    break;
+}
